bfe05647f4be40bdbba5c2ab423dcbcde2a71f9d
Nick Husher <nhusher@gmail.com>
2013-11-06 18:41:29 -0500

    FIXED SOUND

--- src/scripts/mp3.sh
+++ src/scripts/mp3.sh
@@ -4,13 +4,13 @@ echo ""
 echo "Generating base64 encoded MP3 files..."
 echo -n "var MP3=[\"data:audio/mp3;base64," > ../build/mp3-x.js
 
-base64 -e ../src/laser.mp3 >> ../build/mp3-x.js
+base64 ../src/laser.mp3 >> ../build/mp3-x.js
 echo -n "\",\"data:audio/mp3;base64," >> ../build/mp3-x.js
 
-base64 -e ../src/thrust.mp3 >> ../build/mp3-x.js
+base64 ../src/thrust.mp3 >> ../build/mp3-x.js
 echo -n "\",\"data:audio/mp3;base64," >> ../build/mp3-x.js
 
-base64 -e ../src/death.mp3 >> ../build/mp3-x.js
+base64 ../src/death.mp3 >> ../build/mp3-x.js
 echo -n "\",\"data:audio/mp3;base64," >> ../build/mp3-x.js
 
 echo -n "\"];" >> ../build/mp3-x.js
